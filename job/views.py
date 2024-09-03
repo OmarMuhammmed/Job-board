a75f0351a4d04cfabc1613102dd1e3b55476b1aa
OmarMuhammed <amorey2006@gmail.com>
2024-09-03 11:21:17 +0300

    'save_edits'

--- job/views.py
+++ job/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import Job
 from django.core.paginator import Paginator
-from .form import Applyform,Jobform
+from .form import Applyform, Jobform
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from .filters import  JobFilter 
